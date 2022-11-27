faf670b63647dfa01929faf05e47fde9f3cdfeca
m-l-kang <melissa@kang-ohana.com>
2022-11-27 15:57:08 -0800

    made changes in how to load in dataset

--- analysis/EDA/inital_EDA.py
+++ analysis/EDA/inital_EDA.py
@@ -2,10 +2,8 @@
 import pandas as pd
 import numpy as np
 import pickle
-import os
 
 data_path = 'data/cifar-10-batches-py'
-batch_1_filename = os.path.join(*data_path, 'data_batch_1')
 
 def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
     with open(cifar10_dataset_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
